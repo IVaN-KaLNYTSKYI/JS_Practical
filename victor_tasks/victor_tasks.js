59d8fe2592ac20c6e570a06c25e482621d0a4646
macbook <ivan.kalnytskiy@gmail.com>
2021-04-18 17:04:43 +0300

    setTimeout

--- victor_tasks/victor_tasks.js
+++ victor_tasks/victor_tasks.js
@@ -141,7 +141,6 @@ function count(namber){
 console.log(count(2176419194758610021))*/
 
 
-
 /*                                                       mentor                                 * /
 /*
 
@@ -222,7 +221,6 @@ console.log(filtr(users,{name:"Max",age:16}))
 
 const filter = (arr, obj) => arr.filter(value => Object.keys(obj).every(e => obj[e] === value[e]));
 console.log(filter(users, {age: 16}));*/
-
 
 
 /*
@@ -350,3 +348,31 @@ const setData= ()=>{
     document.body.appendChild(btn);
     newDiv.classList.add('border');
 };*/
+
+
+
+
+/*Реалізувати друкарську машинку.
+    У вас є текст "Hello World".
+    Ваша функція має друкувати цей текст по 1 симоволу в браузері.
+    КОЖНА нова буква має бути з РАНДОМНОЮ заутримкою від 0.1 до 1 секунди.
+    Цим самим ви маєте симулювати написання даного тексту юзером.
+    Приклад: "Hello"
+Затримки:
+    H (затримка 0.6)
+e (затримка 0.1)
+l (затримка 0.3)
+l (затримка 0.7)
+о (затримка 1)*/
+let a = "hello"
+const arr = a.split("");
+console.log(arr)
+
+function liter(arr) {
+    for (let i of arr) {
+        setTimeout(() => {
+            document.write(`${i}<br>`)
+        }, Math.floor(Math.random() * 1000)+100)
+    }
+}
+liter(arr)
